febf289a60b227c299df3582a82a4c6c0fa12bff
yakovkurochkin <yakovkurochkin@gmail.com>
2016-02-25 14:00:24 +0000

    fist task

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+if  [!= -f /etc/nginx/sites-enabled/default ]; then
+sudo rm /etc/nginx/sites-enabled/default
+fi
+
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
