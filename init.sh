9d6b613bc267e4684105a42efcbb42e3c41f827c
Yakov <yakovkurochin@gmail.com>
2016-02-25 14:29:46 +0000

    bugfix

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-if  [!= -f /etc/nginx/sites-enabled/default ]; then
+
 sudo rm /etc/nginx/sites-enabled/default
-fi
 
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
